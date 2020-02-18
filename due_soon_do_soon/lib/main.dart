9d8649db3a0e47718d50da7384d100a281645300
Ethan Liao <marineethan@gmail.com>
2020-02-18 04:51:44 -0800

    add first activity
    - added "homepage" view
    - added listview w/ reminder components
    - added add reminder button

--- due_soon_do_soon/lib/main.dart
+++ due_soon_do_soon/lib/main.dart
@@ -3,25 +3,38 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue[500],
+          title: Text("Due Soon, Do Soon"),
+        ),
+
+        body: ListView.builder(
+          itemCount: 8,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                onTap: () {},
+                title: Text("Reminder"),
+                trailing: Icon(
+                  Icons.check_box,
+                  color: Colors.blue[800],
+                  size: 24.0,
+                )
+              )
+            );
+          }
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text("+"),
+          backgroundColor: Colors.blue[900],
+        ),
+        ));
   }
 }
 
